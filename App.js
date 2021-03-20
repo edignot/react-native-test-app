9bbe89da1a7f5b9d5af6a0e479f27e57eed689fa
Ed Ignot <edignot@gmail.com>
2021-03-20 14:11:58 -0600

    update

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import React from "react"
+import { Button, StyleSheet, Text, View } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Edita!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#bbb",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
